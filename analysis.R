825bbd55787967c01f66bef818c92455150651dd
David Yin <yin.dydx@gmail.com>
2017-03-07 21:39:35 -0800

    added code

--- analysis.R
+++ analysis.R
@@ -5,43 +5,45 @@ library(readxl)
 
 
 setwd("~/Desktop/STAT 306/proj/LandSure Data copy") # change this
+
+readsheet <- function(i, j) {
+  temp <- read_excel(paste0(i, ".xlsx"), sheet = j)
+  temp <- temp[,c(2,3,4,5,8,15,16,22)]
+  temp$Category <- as.character(temp$Category)
+  if ("LT Filing" %in% temp$Category) {
+    temp <- subset(temp, Category == "LT Filing")
+    temp$month = i
+    allData <- rbind(allData, temp)
+  }
+}
+
 allData = c()
 for (i in c("04-14","05-14", "06-14", "07-14", "08-14", "09-14", "10-14", "11-14", "12-14", 
             "01-15", "02-15", "03-15", "04-15", "05-15", "06-15", "07-15", "08-15", "09-15", "10-15", "11-15", "12-15", 
             "01-16", "02-16", "03-16", "04-16", "05-16", "06-16", "07-16", "08-16", "09-16", "10-16", "11-16", "12-16",
             "01-17")) {
-  temp <- read_excel(paste0(i, ".xlsx"), sheet = 1)
-  temp <- temp[,c(2,3,4,5,8,15,16,22)]
-  temp[,1] <- as.character(temp[,1])
-  names(temp)[1] = "category"
-  temp <- subset(temp, category == "LT Filing")
-  temp$month = i
+  readsheet(i, 1)
   
-  allData <- rbind(allData, temp)
+  print(paste(paste("ran",i), "1"))
 
-  temp <- read_excel(paste0(i, ".xlsx"), sheet = 3)
-  temp <- temp[,c(2,3,4,5,8,15,16,22)]
-  temp[,1] <- as.character(temp[,1])
-  names(temp)[1] = "category"
-  temp <- subset(temp, category == "LT Filing")
-  temp$month = i
+  readsheet(i, 3)
   
-  allData <- rbind(allData, temp)
+  print(paste(paste("ran",i), "3"))
+  
   
   # 03-15 has bad sheet 4
-  if (i %in% c("04-14","05-14", "06-14", "07-14", "08-14", "09-14", "10-14", "11-14", "12-14", "01-15", "02-15", "04-15", "05-15", "06-15", "07-15", "08-15", "09-15", "10-15", "11-15", "12-15")) {
-    temp <- read_excel(paste0(i, ".xlsx"), sheet = 4)
-    temp <- temp[,c(2,3,4,5,8,15,16,22)]
-    temp[,1] <- as.character(temp[,1])
-    names(temp)[1] = "category"
-    temp <- subset(temp, category == "LT Filing")
-    temp$month = i
-    allData <- rbind(allData, temp)
+  if (i %in% c("04-14","05-14", "06-14", "07-14", "08-14", "09-14", "10-14", "11-14", "12-14", 
+               "01-15", "02-15", "04-15", "05-15", "06-15", "07-15", "08-15", "09-15", "10-15", "11-15", "12-15")) {
+    readsheet(i, 4)
+    
+    print(paste(paste("ran",i), "4"))
+    
   }
   
   ## do other code here
 
 }
+
 setwd("~/Desktop/STAT 306/proj") # change this
 write.csv(allData, file = "allData.csv")
 
