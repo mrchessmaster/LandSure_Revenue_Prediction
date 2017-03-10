92a12ccb69dc5764312ab7baa009e09d688d35c5
David Yin <yin.dydx@gmail.com>
2017-03-09 23:46:12 -0800

    fixed merge

--- merge_data.R
+++ merge_data.R
@@ -6,6 +6,8 @@ library(readxl)
 
 setwd("~/Desktop/STAT 306/proj/LandSure Data copy") # change this
 
+allData = c()
+
 readsheet <- function(i, j) {
   temp <- read_excel(paste0(i, ".xlsx"), sheet = j)
   temp <- temp[,c(2,3,4,5,8,15,16,22)]
@@ -13,19 +15,18 @@ readsheet <- function(i, j) {
   if ("LT Filing" %in% temp$Category) {
     temp <- subset(temp, Category == "LT Filing")
     temp$month = i
-    allData <- rbind(allData, temp)
   }
+  return(temp)
 }
 
-allData = c()
 for (i in c("04-14","05-14", "06-14", "07-14", "08-14", "09-14", "10-14", "11-14", "12-14", 
             "01-15", "02-15", "03-15", "04-15", "05-15", "06-15", "07-15", "08-15", "09-15", "10-15", "11-15", "12-15", 
             "01-16", "02-16", "03-16", "04-16", "05-16", "06-16", "07-16", "08-16", "09-16", "10-16", "11-16", "12-16",
             "01-17")) {
-  readsheet(i, 1)
+  allData <- rbind(allData, readsheet(i, 1))
   print(paste(paste("ran",i), "1"))
 
-  readsheet(i, 3)
+  allData <- rbind(allData, readsheet(i, 3))
   print(paste(paste("ran",i), "3"))
   
   # WHY IS THE DATA SO TERRIBLE????
@@ -35,7 +36,7 @@ for (i in c("04-14","05-14", "06-14", "07-14", "08-14", "09-14", "10-14", "11-14
   
   if (i %in% c("04-14","05-14", "06-14", "07-14", "08-14", "09-14", "10-14", "11-14", "12-14", 
                "01-15", "02-15", "04-15", "05-15", "06-15", "07-15", "09-15", "11-15", "12-15")) {
-    readsheet(i, 4)
+    allData <- rbind(allData, readsheet(i, 4))
     print(paste(paste("ran",i), "4"))
     
   }
@@ -43,6 +44,7 @@ for (i in c("04-14","05-14", "06-14", "07-14", "08-14", "09-14", "10-14", "11-14
   ## do other code here
 
 }
+
 
 setwd("~/Desktop/STAT 306/proj") # change this
 write.csv(allData, file = "allData.csv")
